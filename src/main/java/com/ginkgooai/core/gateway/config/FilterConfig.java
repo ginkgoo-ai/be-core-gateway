b8abd0771ac109eaa334c894ce4bcb5d9fa3fcd5
Wang <645565009@qq.com>
2025-03-03 17:59:56 +0800

    add traces

--- src/main/java/com/ginkgooai/core/gateway/config/FilterConfig.java
+++ src/main/java/com/ginkgooai/core/gateway/config/FilterConfig.java
@@ -6,17 +6,22 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.web.multipart.support.MultipartFilter;
 
 @Configuration
 public class FilterConfig {
 
+    /**
+     * Creates and configures a FilterRegistrationBean for the CachingRequestBodyFilter.
+     * This filter is registered to intercept all requests and is set to the highest precedence.
+     *
+     * @return A FilterRegistrationBean configured for the CachingRequestBodyFilter.
+     */
     @Bean
     public FilterRegistrationBean<CachingRequestBodyFilter> cachingRequestBodyFilter() {
         FilterRegistrationBean<CachingRequestBodyFilter> registration = new FilterRegistrationBean<>();
         registration.setFilter(new CachingRequestBodyFilter());
         registration.addUrlPatterns("/*");
-        registration.setOrder(Ordered.HIGHEST_PRECEDENCE); // 最高优先级
+        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
         return registration;
     }
 
